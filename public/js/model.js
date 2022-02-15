09deedcb3d545f58bbe9de180772fb386465e5a3
Aiden Woodruff <aiden.woodruff@gmail.com>
2022-02-15 10:48:40 -0600

    Extracted forest class

--- public/js/model.js
+++ public/js/model.js
@@ -1,5 +1,6 @@
 import { Tile } from "./tiles.js";
 import { Land } from "./land.js";
+import { Forest } from "./forest.js";
 
 class Organism {
 	constructor (land, id) {
@@ -30,69 +31,6 @@ class Organism {
 	// Virtual?
 	imagineGrowth () {
 
-	}
-}
-class Forest {
-	constructor (land, id) {
-		this.land = land;
-
-		// Sorted lists of ids.
-		this.ids = [id];
-		this.borders = this.land.aroundCardinal(id);
-	}
-
-	// Returns list of ids to claim.
-	grow () {
-		let borders = this.borders.filter(() => {
-			return Math.random() * 100 < 20;
-		});
-
-		return borders;
-	}
-
-	// Returns a single merged forest. Clears this and otherForest.
-	merge (otherForest) {
-		this.ids.push(...otherForest.ids);
-		otherForest.ids = [];
-
-		this.ids.sort((x, y) => x < y ? -1 : (x === y ? 0 : 1));
-
-		this.borders.push(...otherForest.borders);
-		otherForest.borders = [];
-
-		this.borders = this.borders.filter(x => this.contains(x));
-	}
-
-	// strict i.e. true = all, false = any.
-	contains (ids, strict) {
-		if (!Array.isArray(ids)) ids = [ids];
-
-		let stop = !strict;
-		
-		/*const search = (id) => {
-			(function binarySearch (value, array, start, length) {
-				if (array[start] === value) return 
-			})(id, this.ids, 0, this.ids.length);
-		};*/
-
-		for (let i = 0; i < ids.length; ++i) {
-			if (this.ids.includes(ids[i]) === stop) return stop;
-		}
-		return !stop;
-	}
-
-	// Unclaim an id.
-	remove (id) {}
-
-	claim (ids) {
-		this.ids.push(...ids);
-		this.ids.sort((x, y) => x < y ? -1 : (x === y ? 0 : 1));
-
-		this.borders = this.borders.filter(id => !ids.includes(id));
-
-		let borders = ids.flatMap(id => this.land.aroundCardinal(id)).filter(id => !this.contains(id));
-
-		this.borders.push(...borders);
 	}
 }
 
